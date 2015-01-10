32375c0f4eb8af94fa9421d1ebbbf0916743fd1a
David Gutierrez <dgutierr@redhat.com>
2015-01-10 14:54:27 +0100

    BZ-1180440 - Perspective Editor: "DisplayerScreen component" modal window does not correspond with used styles

--- dashbuilder-client/dashbuilder-displayer-editor/src/main/java/org/dashbuilder/client/editor/DisplayerPerspectiveEditorComponent.java
+++ dashbuilder-client/dashbuilder-displayer-editor/src/main/java/org/dashbuilder/client/editor/DisplayerPerspectiveEditorComponent.java
@@ -74,5 +74,6 @@ public class DisplayerPerspectiveEditorComponent implements ExternalPerspectiveE
     public void modalSettings(Modal popup) {
         popup.setWidth(900);
         popup.setMaxHeigth("500px");
+        popup.setTitle("Edit Displayer");
     }
 }
