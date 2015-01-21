8cf3b3006e569155acd1323cd163242288a80d73
David Gutierrez <dgutierr@redhat.com>
2015-01-21 18:49:53 +0100

    Data set lookup javadoc

--- dashbuilder-shared/dashbuilder-dataset-api/src/main/java/org/dashbuilder/dataprovider/DataSetProvider.java
+++ dashbuilder-shared/dashbuilder-dataset-api/src/main/java/org/dashbuilder/dataprovider/DataSetProvider.java
@@ -42,6 +42,30 @@ public interface DataSetProvider {
     /**
      * Fetch a data set and optionally apply several operations (filter, sort, group, ...) on top of it.
      *
+     * <p>For group operations on date columns the grouped column values must stick to the following format:</p>
+     *
+     * <pre>
+     * GroupStrategy   IntervalType       Output
+     * ---------------------------------------------------
+     *   DYNAMIC         SECOND        yyyy-MM-dd HH:mm:ss
+     *   DYNAMIC         MINUTE        yyyy-MM-dd HH:mm
+     *   DYNAMIC         HOUR          yyyy-MM-dd HH
+     *   DYNAMIC         DAY           yyyy-MM-dd
+     *   DYNAMIC         MONTH         yyyy-MM
+     *   DYNAMIC         YEAR          yyyy
+     *
+     *   yyyy=year                 HH=hour from 00 to 23,
+     *   MM=month from 01 to 12    mm=minutes from 00 to 59
+     *   dd=day from 01 to 31      ss=seconds from 00 to 59
+     *
+     *   FIXED           SECOND        From 0 to 59
+     *   FIXED           MINUTE        From 0 to 59
+     *   FIXED           HOUR          From 0 to 23
+     *   FIXED           DAY_OF_WEEK   From 1 to 7 (1=Sunday, 7=Saturday)
+     *   FIXED           MONTH         From 1 to 12 (1=January, 12=December)
+     *   FIXED           QUARTER       From 1 to 4 (1=Ene,Feb,Mar, 4=Oct,Nov,Dec)
+     * </pre>
+     *
      * @param def The data set definition lookup request
      * @param lookup The lookup request over the data set. If null then return the data set as is.
      * @return The resulting data set instance
