12b29c4b9761e9a5303e41b18b88bf043e848911
Roger Martinez <roger600@gmail.com>
2015-02-09 20:56:13 +0100

    Metrics dashboard - Refactoring

--- dashbuilder-webapp/src/main/java/org/dashbuilder/client/gallery/GalleryTree.java
+++ dashbuilder-webapp/src/main/java/org/dashbuilder/client/gallery/GalleryTree.java
@@ -440,6 +440,6 @@ public class GalleryTree {
         nodeList.add(new GalleryPlaceRequest("Sales reports", createPlaceRequest("salesReports")));
         nodeList.add(new GalleryPlaceRequest("Expense reports", createPlaceRequest("expenseReports")));
         nodeList.add(new GalleryPlaceRequest("System metrics (real-time)", createPlaceRequest("metrics_realtime")));
-        nodeList.add(new GalleryPlaceRequest("System metrics (analytic)", createPlaceRequest("metrics_analytic")));
+        nodeList.add(new GalleryPlaceRequest("System metrics (historic)", createPlaceRequest("metrics_analytic")));
     }
 }
