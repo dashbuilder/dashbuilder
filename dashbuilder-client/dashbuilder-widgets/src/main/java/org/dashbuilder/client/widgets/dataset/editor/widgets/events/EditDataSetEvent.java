147f3a7170fa18295015a9e463fac52cc48c3d2b
Jan Hrcek <jhrcek@redhat.com>
2015-05-11 12:54:26 +0200

    Add missing generic params in DataSetEditor events

--- dashbuilder-client/dashbuilder-widgets/src/main/java/org/dashbuilder/client/widgets/dataset/editor/widgets/events/EditDataSetEvent.java
+++ dashbuilder-client/dashbuilder-widgets/src/main/java/org/dashbuilder/client/widgets/dataset/editor/widgets/events/EditDataSetEvent.java
@@ -6,13 +6,13 @@ package org.dashbuilder.client.widgets.dataset.editor.widgets.events;
 public class EditDataSetEvent extends AbstractDataSetEvent<EditDataSetEventHandler> {
 
     public static Type<EditDataSetEventHandler> TYPE = new Type<EditDataSetEventHandler>();
-    
+
     public EditDataSetEvent(String uuid) {
         super(uuid);
     }
 
     @Override
-    public Type getAssociatedType() {
+    public Type<EditDataSetEventHandler> getAssociatedType() {
         return TYPE;
     }
 
@@ -20,5 +20,4 @@ public class EditDataSetEvent extends AbstractDataSetEvent<EditDataSetEventHandl
     protected void dispatch(EditDataSetEventHandler handler) {
         handler.onEditDataSet(this);
     }
-
 }
