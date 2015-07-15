a4eed42cf23c265ae6d55b6dff462135de6b240c
David Gutierrez <dgutierr@redhat.com>
2015-07-15 18:59:36 +0200

    Log any backend error caught by the ExceptionManager

--- dashbuilder-backend/dashbuilder-services/src/main/java/org/dashbuilder/dataset/backend/exception/ExceptionManager.java
+++ dashbuilder-backend/dashbuilder-services/src/main/java/org/dashbuilder/dataset/backend/exception/ExceptionManager.java
@@ -24,7 +24,7 @@ public class ExceptionManager {
      * @return The portable exception to send to the client side.
      */
     public RuntimeException handleException(final Exception e) {
-        log.debug(e.getMessage(), e);
+        log.error(e.getMessage(), e);
         if (e instanceof RuntimeException && EnvUtil.isPortableType(e.getClass()) ) {
             return (RuntimeException) e;
         }
