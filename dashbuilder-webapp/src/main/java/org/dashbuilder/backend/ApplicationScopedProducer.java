b73632e6cb7070e061d45db7e298332002cd4adc
Roger Martinez <roger600@gmail.com>
2015-10-17 23:27:12 +0200

    DASHBUILDE-56

--- dashbuilder-webapp/src/main/java/org/dashbuilder/backend/ApplicationScopedProducer.java
+++ dashbuilder-webapp/src/main/java/org/dashbuilder/backend/ApplicationScopedProducer.java
@@ -1,7 +1,13 @@
 package org.dashbuilder.backend;
 
-import java.net.URI;
-import java.util.HashMap;
+import org.jboss.errai.security.shared.api.identity.User;
+import org.jboss.errai.security.shared.service.AuthenticationService;
+import org.uberfire.backend.server.IOWatchServiceAllImpl;
+import org.uberfire.commons.services.cdi.Startup;
+import org.uberfire.commons.services.cdi.StartupType;
+import org.uberfire.io.IOService;
+import org.uberfire.io.impl.IOServiceNio2WrapperImpl;
+
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.context.RequestScoped;
@@ -9,22 +15,7 @@ import javax.enterprise.inject.Produces;
 import javax.inject.Inject;
 import javax.inject.Named;
 
-import org.jboss.errai.security.shared.api.identity.User;
-import org.jboss.errai.security.shared.service.AuthenticationService;
-import org.uberfire.backend.server.IOWatchServiceAllImpl;
-import org.uberfire.backend.server.IOWatchServiceNonDotImpl;
-import org.uberfire.commons.cluster.ClusterServiceFactory;
-import org.uberfire.commons.services.cdi.Startup;
-import org.uberfire.commons.services.cdi.StartupType;
-import org.uberfire.io.IOService;
-import org.uberfire.io.impl.IOServiceDotFileImpl;
-import org.uberfire.io.impl.IOServiceNio2WrapperImpl;
-import org.uberfire.io.impl.cluster.IOServiceClusterImpl;
-import org.uberfire.java.nio.file.FileSystem;
-import org.uberfire.java.nio.file.FileSystemAlreadyExistsException;
-import org.uberfire.java.nio.file.FileSystemNotFoundException;
-
-@Startup(StartupType.BOOTSTRAP)
+@Startup(value = StartupType.BOOTSTRAP)
 @ApplicationScoped
 public class ApplicationScopedProducer {
 
@@ -34,51 +25,17 @@ public class ApplicationScopedProducer {
     @Inject
     private IOWatchServiceAllImpl watchService;
 
-    @Inject
-    @Named("clusterServiceFactory")
-    private ClusterServiceFactory clusterServiceFactory;
-
     private IOService ioService;
-
-    @Inject
-    @Named("configIO")
-    private IOService configIO;
-
-    private FileSystem systemFS;
 
     @PostConstruct
     public void setup() {
-        if ( clusterServiceFactory == null ) {
-            ioService = new IOServiceDotFileImpl(watchService);
-        } else {
-            ioService = new IOServiceClusterImpl(new IOServiceDotFileImpl(watchService), clusterServiceFactory);
-        }
-        
-        // NOTE: The creation of the system git repository should be done by uberfire itself, but
-        // currently exist a bug on UF about it -> the system git repository is not created by uf.
-        // So as a temporary workaround, let's ensure that it's created at application startup. 
-        try {
-            systemFS = configIO.newFileSystem( URI.create( "git://system" ),
-                    new HashMap<String, Object>() {{
-                        put( "init", Boolean.TRUE );
-                        put( "internal", Boolean.TRUE );
-                    }} );
-        }  catch ( final FileSystemAlreadyExistsException ignore ) {
-            systemFS = ioService.getFileSystem( URI.create( "git://system" ) );
-        }
-        
+        ioService  = new IOServiceNio2WrapperImpl("1", watchService);
     }
 
     @Produces
     @Named("ioStrategy")
     public IOService ioService() {
         return ioService;
-    }
-
-    @Produces
-    @Named("systemFS")
-    public FileSystem systemFS() {
-        return systemFS;
     }
 
     @Produces
