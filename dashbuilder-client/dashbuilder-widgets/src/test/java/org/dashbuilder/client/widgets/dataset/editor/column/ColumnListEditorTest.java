a664d9cc8052fb2d57c08bded9f9f85f24b71e02
Roger Martinez <roger600@gmail.com>
2015-12-10 19:37:05 +0100

    DASHBUILDE-79: DataSetEditor content disappears

--- dashbuilder-client/dashbuilder-widgets/src/test/java/org/dashbuilder/client/widgets/dataset/editor/column/ColumnListEditorTest.java
+++ dashbuilder-client/dashbuilder-widgets/src/test/java/org/dashbuilder/client/widgets/dataset/editor/column/ColumnListEditorTest.java
@@ -311,6 +311,29 @@ public class ColumnListEditorTest {
         verify(view, times(0)).clear();
     }
 
+    /**
+     * See https://issues.jboss.org/browse/DASHBUILDE-79
+     */
+    @Test
+    public void testDataColumnDefEditorSource_Create_DASHBUILDE79() {
+        final DataColumnDef col2 = mock(DataColumnDef.class);
+        when(col2.getId()).thenReturn("col2");
+        when(col2.getColumnType()).thenReturn(ColumnType.LABEL);
+        presenter.acceptableColumns.add(col2);
+        presenter.listEditor.getList().add(col2);
+        presenter.listEditor.getEditors().clear();
+        presenter.restrictedColumns.clear();
+        ColumnListEditor.DataColumnDefEditorSource source = presenter.createDataColumnDefEditorSource();
+        org.dashbuilder.dataset.client.editor.DataColumnDefEditor e = source.create(0);
+        verify(dataColumnDefEditor, times(1)).isEditMode(true);
+        verify(dataColumnDefEditor, times(1)).setOriginalColumnType(any(ColumnType.class));
+        verify(dataColumnDefEditor, times(1)).setProviderType(any(DataSetProviderType.class));
+        verify(view, times(1)).insert(anyInt(), any(DataColumnDefEditor.View.class), anyBoolean(), anyBoolean(), anyString());
+        verify(view, times(1)).remove(anyInt());
+        verify(view, times(0)).init(presenter);
+        verify(view, times(0)).clear();
+    }
+
 
     /**
      * Ensure create a dummy not editable column when disposing the only one present from the definition.
