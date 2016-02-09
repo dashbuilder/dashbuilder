d915f729aa5ae3c4967af0979f262b06b9185b41
David Gutierrez <dgutierr@redhat.com>
2016-02-09 10:51:58 +0100

    Nav header fine tuning

--- dashbuilder-webapp/src/main/java/org/dashbuilder/client/ShowcaseEntryPoint.java
+++ dashbuilder-webapp/src/main/java/org/dashbuilder/client/ShowcaseEntryPoint.java
@@ -16,22 +16,26 @@
 package org.dashbuilder.client;
 
 import com.google.gwt.animation.client.Animation;
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.dom.client.Style;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.Widget;
 import org.dashbuilder.client.dashboard.DashboardManager;
 import org.dashbuilder.client.dashboard.DashboardPerspectiveActivity;
 import org.dashbuilder.client.dashboard.widgets.NewDashboardForm;
-import org.dashbuilder.client.resources.AppResource;
 import org.dashbuilder.client.resources.i18n.AppConstants;
 import org.dashbuilder.shared.dashboard.events.DashboardCreatedEvent;
 import org.dashbuilder.shared.dashboard.events.DashboardDeletedEvent;
-import org.gwtbootstrap3.client.ui.Image;
+import org.jboss.errai.common.client.api.Caller;
+import org.jboss.errai.common.client.api.RemoteCallback;
+import org.jboss.errai.ioc.client.api.AfterInitialization;
 import org.jboss.errai.ioc.client.api.EntryPoint;
+import org.jboss.errai.security.shared.api.Role;
+import org.jboss.errai.security.shared.api.identity.User;
+import org.jboss.errai.security.shared.service.AuthenticationService;
 import org.uberfire.client.mvp.PlaceManager;
-import org.uberfire.client.views.pfly.menu.MainBrand;
-import org.uberfire.client.workbench.events.ApplicationReadyEvent;
+import org.uberfire.client.views.pfly.menu.UserMenu;
+import org.uberfire.client.workbench.widgets.menu.UtilityMenuBar;
 import org.uberfire.client.workbench.widgets.menu.WorkbenchMenuBar;
 import org.uberfire.mvp.Command;
 import org.uberfire.mvp.impl.DefaultPlaceRequest;
@@ -40,11 +44,8 @@ import org.uberfire.workbench.model.menu.MenuFactory;
 import org.uberfire.workbench.model.menu.MenuItem;
 import org.uberfire.workbench.model.menu.Menus;
 
-import javax.annotation.PostConstruct;
-import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Event;
 import javax.enterprise.event.Observes;
-import javax.enterprise.inject.Produces;
 import javax.inject.Inject;
 import java.util.ArrayList;
 import java.util.List;
@@ -58,11 +59,22 @@ import static org.uberfire.workbench.model.menu.MenuFactory.newTopLevelMenu;
 @EntryPoint
 public class ShowcaseEntryPoint {
 
+    private AppConstants constants = AppConstants.INSTANCE;
+
     @Inject
     private WorkbenchMenuBar menubar;
 
     @Inject
     private PlaceManager placeManager;
+
+    @Inject
+    public User identity;
+
+    @Inject
+    private UserMenu userMenu;
+
+    @Inject
+    private UtilityMenuBar utilityMenuBar;
 
     @Inject
     private DashboardManager dashboardManager;
@@ -71,38 +83,68 @@ public class ShowcaseEntryPoint {
     private NewDashboardForm newDashboardForm;
 
     @Inject
+    private Caller<AuthenticationService> authService;
+
+    @Inject
     private Event<NotificationEvent> workbenchNotification;
 
-    @PostConstruct
+    @AfterInitialization
     public void startApp() {
+        setupMenus();
         hideLoadingPopup();
     }
 
-    private void setupMenu( @Observes final ApplicationReadyEvent event ) {
+    private void setupMenus() {
+        for (Menus roleMenus : getRoles()) {
+            userMenu.addMenus(roleMenus);
+        }
+        refreshMenus();
+    }
+
+    private void refreshMenus() {
+        menubar.clear();
         menubar.addMenus(createMenuBar());
+
+        final Menus utilityMenus =
+                MenuFactory.newTopLevelCustomMenu(userMenu)
+                        .endMenu()
+                        .build();
+
+        utilityMenuBar.addMenus(utilityMenus);
     }
 
     private Menus createMenuBar() {
-        return newTopLevelMenu(AppConstants.INSTANCE.menu_home())
+        return newTopLevelMenu(constants.menu_home())
                 .place(new DefaultPlaceRequest("HomePerspective"))
                 .endMenu().
-                newTopLevelMenu(AppConstants.INSTANCE.menu_gallery())
+                newTopLevelMenu(constants.menu_gallery())
                 .place(new DefaultPlaceRequest("DisplayerGalleryPerspective")).endMenu().
-                newTopLevelMenu(AppConstants.INSTANCE.menu_authoring())
+                newTopLevelMenu(constants.menu_authoring())
                 .withItems(getAuthoringMenuItems())
                 .endMenu().
-                newTopLevelMenu(AppConstants.INSTANCE.menu_dashboards())
+                newTopLevelMenu(constants.menu_dashboards())
                 .withItems(getDashboardMenuItems())
                 .endMenu().
-                newTopLevelMenu(AppConstants.INSTANCE.menu_extensions())
+                newTopLevelMenu(constants.menu_extensions())
                 .withItems(getExtensionsMenuItems())
                 .endMenu().
                 build();
     }
 
+    private List<Menus> getRoles() {
+        final List<Menus> result = new ArrayList<Menus>(identity.getRoles().size());
+        result.add(MenuFactory.newSimpleItem(constants.logOut()).respondsWith(new LogoutCommand()).endMenu().build());
+        for (Role role : identity.getRoles()) {
+            if (!role.getName().equals( "IS_REMEMBER_ME")) {
+                result.add(MenuFactory.newSimpleItem(constants.role() + ": " + role.getName() ).endMenu().build());
+            }
+        }
+        return result;
+    }
+
     private List<? extends MenuItem> getAuthoringMenuItems() {
-        final List<MenuItem> result = new ArrayList<>(2);
-        result.add(newMenuItem(AppConstants.INSTANCE.menu_dataset_authoring(), "DataSetAuthoringPerspective"));
+        final List<MenuItem> result = new ArrayList(2);
+        result.add(newMenuItem(constants.menu_dataset_authoring(), "DataSetAuthoringPerspective"));
         return result;
     }
 
@@ -110,13 +152,13 @@ public class ShowcaseEntryPoint {
         final List<MenuItem> result = new ArrayList<>(2);
 
         // Add the new dashboard creation link
-        result.add(MenuFactory.newSimpleItem(AppConstants.INSTANCE.menu_dashboards_new())
+        result.add(MenuFactory.newSimpleItem(constants.menu_dashboards_new())
                 .respondsWith(getNewDashboardCommand())
                 .endMenu().build().getItems().get(0));
 
         // Add hard-coded dashboard samples
-        result.add(newMenuItem(AppConstants.INSTANCE.menu_dashboards_salesdb(), "SalesDashboardPerspective"));
-        result.add(newMenuItem(AppConstants.INSTANCE.menu_dashboards_salesreports(), "SalesReportsPerspective"));
+        result.add(newMenuItem(constants.menu_dashboards_salesdb(), "SalesDashboardPerspective"));
+        result.add(newMenuItem(constants.menu_dashboards_salesreports(), "SalesReportsPerspective"));
 
         // Add dashboards created in runtime
         for (DashboardPerspectiveActivity activity : dashboardManager.getDashboards()) {
@@ -142,23 +184,22 @@ public class ShowcaseEntryPoint {
     }
 
     private void onDashboardCreatedEvent(@Observes DashboardCreatedEvent event) {
-        menubar.clear();
-        menubar.addMenus(createMenuBar());
+        refreshMenus();
+
         // Navigate to the activity after rebuilding the menu bar entries, if not, the activity perspective menus are override by uf and they do not appear.
         placeManager.goTo(new DefaultPlaceRequest(event.getDashboardId()));
-        workbenchNotification.fire(new NotificationEvent(AppConstants.INSTANCE.notification_dashboard_created(event.getDashboardName()), INFO));
+        workbenchNotification.fire(new NotificationEvent(constants.notification_dashboard_created(event.getDashboardName()), INFO));
     }
 
     private void onDashboardDeletedEvent(@Observes DashboardDeletedEvent event) {
-        menubar.clear();
-        menubar.addMenus(createMenuBar());
-        workbenchNotification.fire(new NotificationEvent(AppConstants.INSTANCE.notification_dashboard_deleted(event.getDashboardName()), INFO));
+        refreshMenus();
+        workbenchNotification.fire(new NotificationEvent(constants.notification_dashboard_deleted(event.getDashboardName()), INFO));
     }
 
     private List<? extends MenuItem> getExtensionsMenuItems() {
         final List<MenuItem> result = new ArrayList<>(2);
-        result.add(newMenuItem(AppConstants.INSTANCE.menu_extensions_plugins(), "PlugInAuthoringPerspective"));
-        result.add(newMenuItem(AppConstants.INSTANCE.menu_extensions_apps(), "AppsPerspective"));
+        result.add(newMenuItem(constants.menu_extensions_plugins(), "PlugInAuthoringPerspective"));
+        result.add(newMenuItem(constants.menu_extensions_apps(), "AppsPerspective"));
         return result;
     }
     
@@ -190,16 +231,21 @@ public class ShowcaseEntryPoint {
         }.run( 500 );
     }
 
-    @Produces
-    @ApplicationScoped
-    public MainBrand createBrandLogo() {
-        return new MainBrand() {
-            @Override
-            public Widget asWidget() {
-                final Image logo = new Image( AppResource.INSTANCE.images().ufUserLogo() );
-                logo.setSize("200px", "60px");
-                return logo;
-            }
-        };
+    private class LogoutCommand implements Command {
+
+        @Override
+        public void execute() {
+            authService.call(new RemoteCallback<Void>() {
+                @Override
+                public void callback(Void response) {
+                    final String location = GWT.getModuleBaseURL().replaceFirst( "/" + GWT.getModuleName() + "/", "/logout.jsp" );
+                    redirect( location );
+                }
+            }).logout();
+        }
     }
+
+    public static native void redirect( String url )/*-{
+        $wnd.location = url;
+    }-*/;
 }
