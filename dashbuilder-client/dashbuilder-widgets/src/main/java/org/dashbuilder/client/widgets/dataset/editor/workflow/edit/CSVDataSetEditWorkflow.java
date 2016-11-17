848ebc35ecb21e859c90b9fb3b867e49c31466ea
pefernan <pere.fernandez@gmail.com>
2016-11-17 14:20:21 +0100

    DASHBUILDE-102: Upgrade dashbuilder-validations to use Errai Validation

--- dashbuilder-client/dashbuilder-widgets/src/main/java/org/dashbuilder/client/widgets/dataset/editor/workflow/edit/CSVDataSetEditWorkflow.java
+++ dashbuilder-client/dashbuilder-widgets/src/main/java/org/dashbuilder/client/widgets/dataset/editor/workflow/edit/CSVDataSetEditWorkflow.java
@@ -15,42 +15,41 @@
  */
 package org.dashbuilder.client.widgets.dataset.editor.workflow.edit;
 
-import com.google.gwt.editor.client.SimpleBeanEditorDriver;
-import org.dashbuilder.client.widgets.dataset.editor.driver.CSVDataSetDefDriver;
-import org.dashbuilder.client.widgets.dataset.event.CancelRequestEvent;
-import org.dashbuilder.client.widgets.dataset.event.SaveRequestEvent;
-import org.dashbuilder.client.widgets.dataset.event.TestDataSetRequestEvent;
-import org.dashbuilder.dataprovider.DataSetProviderType;
-import org.dashbuilder.dataset.client.DataSetClientServices;
-import org.dashbuilder.dataset.client.editor.CSVDataSetDefEditor;
-import org.dashbuilder.dataset.def.CSVDataSetDef;
-import org.dashbuilder.validations.dataset.DataSetDefValidator;
-import org.jboss.errai.ioc.client.container.SyncBeanManager;
-
 import javax.enterprise.context.Dependent;
 import javax.enterprise.event.Event;
 import javax.inject.Inject;
 import javax.validation.ConstraintViolation;
 
+import com.google.gwt.editor.client.SimpleBeanEditorDriver;
+import org.dashbuilder.client.widgets.dataset.editor.driver.CSVDataSetDefDriver;
+import org.dashbuilder.client.widgets.dataset.event.CancelRequestEvent;
+import org.dashbuilder.client.widgets.dataset.event.SaveRequestEvent;
+import org.dashbuilder.client.widgets.dataset.event.TestDataSetRequestEvent;
+import org.dashbuilder.dataset.client.DataSetClientServices;
+import org.dashbuilder.dataset.client.editor.CSVDataSetDefEditor;
+import org.dashbuilder.dataset.def.CSVDataSetDef;
+import org.dashbuilder.validations.DataSetValidatorProvider;
+import org.jboss.errai.ioc.client.container.SyncBeanManager;
+
 
 /**
  * <p>CSV Data Set Editor workflow presenter for editing a data set definition instance.</p>
  * <p>This class is the main entry point for editing an existing data set definition instance. It links the gwt editors with the given driver to perform flushing and validations.</p>
- * 
- * @since 0.4.0 
+ *
+ * @since 0.4.0
  */
 @Dependent
 public class CSVDataSetEditWorkflow extends DataSetEditWorkflow<CSVDataSetDef, CSVDataSetDefEditor> {
 
     @Inject
     public CSVDataSetEditWorkflow(final DataSetClientServices clientServices,
-                                   final DataSetDefValidator dataSetDefValidator,
+                                   final DataSetValidatorProvider validatorProvider,
                                    final SyncBeanManager beanManager,
                                    final Event<SaveRequestEvent> saveRequestEvent,
                                    final Event<TestDataSetRequestEvent> testDataSetEvent,
                                    final Event<CancelRequestEvent> cancelRequestEvent,
                                    final View view) {
-        super(clientServices, dataSetDefValidator, beanManager, saveRequestEvent, testDataSetEvent, cancelRequestEvent, view);
+        super(clientServices, validatorProvider, beanManager, saveRequestEvent, testDataSetEvent, cancelRequestEvent, view);
     }
 
 
@@ -65,10 +64,14 @@ public class CSVDataSetEditWorkflow extends DataSetEditWorkflow<CSVDataSetDef, C
     }
 
     @Override
-    protected Iterable<ConstraintViolation<?>> validate(boolean isCacheEnabled, boolean isPushEnabled, boolean isRefreshEnabled) {
-        final Iterable<ConstraintViolation<?>> violations = dataSetDefValidator.validatorFor(DataSetProviderType.CSV).validate(dataSetDef,
-                isCacheEnabled, isPushEnabled, isRefreshEnabled, editor.isUsingFilePath());
-        return violations;
+    protected Iterable<ConstraintViolation<?>> validate( boolean isCacheEnabled,
+                                                         boolean isPushEnabled,
+                                                         boolean isRefreshEnabled ) {
+
+        return validatorProvider.validate( dataSetDef,
+                                           isCacheEnabled,
+                                           isPushEnabled,
+                                           isRefreshEnabled, editor.isUsingFilePath() );
     }
 
     @Override
