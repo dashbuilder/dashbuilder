7e37514250499361d3a08a3ecfff02ca7f22590d
Michael Biarnes Kiefer <mbiarnes@redhat.com>
2017-10-11 10:54:59 +0200

    the tags are pushed now to Gerrit instead of jboss-integration

--- scripts/release/dashbuilder-pushTag.sh
+++ scripts/release/dashbuilder-pushTag.sh
@@ -10,11 +10,11 @@ fi
 
 commitMsg="Tagging $tag"
 cd $WORKSPACE/dashbuilder
-# pushes the TAG to github.com:jboss-integration or github.com:dashbuilder [IMPORTANT: "push -n" (--dryrun) should be replaced by "push" when script is ready]
+# pushes the TAG to ssh://jb-ip-tooling-jenkins@code.engineering.redhat.com/dashbuilder [IMPORTANT: "push -n" (--dryrun) should be replaced by "push" when script is ready]
 if [ "$target" == "productized" ]; then
    git tag -a $TAG -m "$commitMsg"
-   git remote add upstream git@github.com:jboss-integration/dashbuilder.git
-   git push upstream $tag
+   git remote add gerrit ssh://jb-ip-tooling-jenkins@code.engineering.redhat.com/dashbuilder
+   git push gerrit $tag
 else
    git tag -a $TAG -m "$commitMsg"
    git push origin $tag
