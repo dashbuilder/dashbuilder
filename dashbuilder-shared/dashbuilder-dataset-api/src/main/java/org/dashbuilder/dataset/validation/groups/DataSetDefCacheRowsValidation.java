de67f7211d7e254ba95b437c644dac4e2b260a1d
Max Barkley <mbarkley@redhat.com>
2017-11-01 14:39:06 -0400

    AF-593+: Move dashbuilder-dataset to kie-soup (#393)

--- dashbuilder-shared/dashbuilder-dataset-api/src/main/java/org/dashbuilder/dataset/validation/groups/DataSetDefCacheRowsValidation.java
+++
@@ -1,8 +0,0 @@
-package org.dashbuilder.dataset.validation.groups;
-
-/**
- * <p>Validation group for the attribute <code>cacheMaxRows</code> of the data set definition class.</p>
- */
-public interface DataSetDefCacheRowsValidation {
-
-}
--- dashbuilder-shared/dashbuilder-dataset-api/src/main/java/org/dashbuilder/dataset/validation/groups/DataSetDefRefreshIntervalValidation.java
+++
@@ -1,7 +0,0 @@
-package org.dashbuilder.dataset.validation.groups;
-
-/**
- * <p>Validation group for the attribute <code>refreshTime</code> of the data set definition class.</p>
- */
-public interface DataSetDefRefreshIntervalValidation {
-}
